c21f03b6ed38a4c0da697fa9b878f0adb40814b3
Roma Bury <92829743+Roma104@users.noreply.github.com>
2024-12-21 12:13:20 +0100

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -5,5 +5,9 @@
 # there is no file with this name on the disk
 file_name = 'xyz.txt'
 
-with open(file_name) as file:
-    content = file.read()
+try:
+    with open(file_name) as file:
+        content = file.read()
+    print(content)
+except FileNotFoundError:
+    print(f'The file {file_name} does not exist')
